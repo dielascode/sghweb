40a3d01aa525bc1437a44f4c53317ec0ece08799
revalino16 <revalinosurya83@gmail.com>
2025-12-04 21:19:06 +0700

    repal4

--- js/loadMain.js
+++ js/loadMain.js
@@ -57,3 +57,29 @@ function sendToWA(e) {
 
     window.open(`https://wa.me/${nomor}?text=${text}`);
 }
+
+
+
+
+// layanan-magang.html
+// const observers = document.querySelectorAll('.fade-up, .slide-left, .slide-right, .slide-up');
+
+// const options = {
+//   threshold: 0.8
+// };
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach(entry => {
+//     if(entry.isIntersecting){
+//       entry.target.classList.add('show');
+//     }
+//   });
+// }, options);
+
+// observers.forEach(el => observer.observe(el));
+
+AOS.init({
+    duration: 800,     // durasi animasi
+    offset: 120,       // jarak scroll sebelum animasi mulai
+    once: true,        // animasi muncul sekali saja
+  });   
